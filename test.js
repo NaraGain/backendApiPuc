525da671367dec7db910d32ef3416247f5eeb25a
nareGain <bunnarachoch@gmail.com>
2024-01-06 00:16:13 +0700

    test 2

--- test.js
+++ test.js
@@ -3,6 +3,14 @@ const server = require('./index'); // Import the server from app.js
 const assert = require('assert')
 const http = require('http')
 
+function yourFunctionToTest(status){
+    if(status !== 'success'){
+        return 'faild'
+    }
+    return status
+}
+
+
 describe('Server Tests', ()=>{
     describe('Your Functionlity Test', ()=>{
         it('should perform a specific action', function (done) {
@@ -11,25 +19,26 @@ describe('Server Tests', ()=>{
       
             // Replace this with your actual test logic
             // For example, if you have an HTTP server running on localhost:3000:
-            http.get('http://localhost:4000', function (res) {
-              // Assuming the response should have a status code of 200
-              assert.strictEqual(res.statusCode, 200);
-              // Call done() to signal the end of this test case
-              done();
-            })
-        })
-        function yourFunctionToTest(num){
-            if(num !== 1){
-                return 'faild'
+        
+            try {
+                http.get('http://localhost:4000', function (res) {
+                    // Assuming the response should have a status code of 200
+                    assert.strictEqual(res.statusCode, 200);
+                    // Call done() to signal the end of this test case
+                    done();
+                  })
+      
+            } catch (error) {
+               yourFunctionToTest(error)
             }
-            return num
-        }
+        })
+      
         // Test case to check another functionality
     it('should do something else', function () {
         // Your test logic here
         // For example, check the output of a function or a module
-        const result = yourFunctionToTest(1);
-        assert.strictEqual(result,1);
+        const result = yourFunctionToTest('success');
+        assert.strictEqual(result,'success');
       });
     })
     server.close()
